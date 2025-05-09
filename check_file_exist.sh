8f7633a95293ee001c1e88cb06e68a94083260e7
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-09 09:26:25 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-# Ce script vérifie si un fichier donné existe dans le répertoire courant.
-
-# Demande du nom du fichier
+# Ce script vérifie si un fichier donné existe.
+ 
+# Demande du nom du fichier à l’utilisateur
 read -p "Entrez le nom du fichier : " filename
-
-# Vérifie si l'entrée n'est pas vide
-if [ -z "$filename" ]; 
-  then
-  echo "Erreur : aucun nom de fichier fourni."
-  exit 1
-fi
-
-# Vérification de l'existence dans le répertoire courant
-if [ -f "./$filename" ]; 
-  then
-  echo "Le fichier '$filename' existe dans le répertoire courant."
+ 
+# Vérification de l'existence
+if [ -f "$filename" ]; then
+ echo "Le fichier '$filename' existe."
 else
-  echo "Le fichier '$filename' n'existe pas dans le répertoire courant."
+ echo "Le fichier '$filename' n'existe pas."
 fi
