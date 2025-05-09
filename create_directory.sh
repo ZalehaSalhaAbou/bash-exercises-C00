fc55a136977d40e030a9759475e6d29e0d0cdc3f
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-09 08:07:31 +0100

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Ce script crée un répertoire nommé test_directory s’il n’existe pas déjà.
- 
+# Ce script crée un répertoire nommé test_directory s’il n’existe pas déjà
 directory_name="test_directory"
- 
 # Vérification de l'existence
 if [ -d "$directory_name" ]; then
- echo "Le répertoire '$directory_name' existe déjà."
+ echo "Le répertoire '$directory_name' existe déjà"
 else
  mkdir "$directory_name"
- echo "Le répertoire '$directory_name' a été créé."
+ echo "Le répertoire '$directory_name' a été créé"
 fi
