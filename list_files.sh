eef592db259b45b5e0772f0b3ed9864e95dde902
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-09 08:10:37 +0100

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Ce script affiche la liste des fichiers du répertoire courant.
- 
+# Ce script affiche la liste des fichiers du répertoire courant
 # Utilisation de ls pour lister uniquement les fichiers
 echo "Liste des fichiers dans le répertoire courant :"
 ls -p | grep -v /
