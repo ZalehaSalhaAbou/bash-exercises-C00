1ad6947ed6cbb69be0774b4ef2e505bbfe8d5c63
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-09 01:52:08 +0100

    Create Exo 2 – current_datetime.sh

---
+++ Exo 2 – current_datetime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ce script affiche la date et l’heure actuelles au format "YYYY-MM-DD HH:MM:SS".
+ 
+# La commande date avec le bon format
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "Date et heure actuelles : $current_time"
